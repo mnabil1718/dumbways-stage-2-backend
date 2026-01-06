4952095d01e0dac9befc331200b877a83fdedbba
mnabil1718 <mnabil1718@gmail.com>
2026-01-06 10:42:50 +0700

    chore(root): tsconfig and lint config

--- apps/store/src/middlewares/error.ts
+++ apps/store/src/middlewares/error.ts
@@ -2,7 +2,7 @@ import { ClientError } from "@repo/shared";
 import { NextFunction, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 
-export const errorHandler = (err: unknown, req: Request, res: Response, next: NextFunction) => {
+export const errorHandler = (err: unknown, req: Request, res: Response, _: NextFunction) => {
         if (err instanceof ClientError) {
                 return res.status(err.statusCode).json({ message: err.message });
         }
